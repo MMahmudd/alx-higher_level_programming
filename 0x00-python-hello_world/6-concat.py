1aabfb78eb5b62585bc80730c5bcbb3bf515caea
MMahmudd <shametlab@gmai.com>
2023-05-10 14:32:55 -0700

    update 6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str11 = "Holberton"
-str22 = "School"
-str11 += " " + str22
-print("Welcome to {}!".format(str11))
+str1 = "Holberton"
+str2 = "School"
+str1 += " " + str2
+print("Welcome to {}!".format(str1))
