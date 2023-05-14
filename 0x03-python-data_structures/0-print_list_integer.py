e5aec6c3ab6ea46fbfc820740c6d0549926d7ebd
MMahmudd <shametlab@gmai.com>
2023-05-14 10:48:39 -0700

    update

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 0-print_list_integer.py
+
+def print_list_integer(my_list=[]):
+    """Printout all integers of the list."""
+    for num in my_list:
+        print("{0}".format(num))
