0550486bcb83f8c82107dd6e00c2e02faf4a5681
MMahmudd <shametlab@gmai.com>
2023-05-18 02:28:50 -0700

    update

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    # Create an empty set to store unique integers
+    unique_set = set()
+
+    numb = 0
+
+    # Iterate over each element in the input list
+    for i in unique_list:
+        numb += i
+        return (numb)
