3cba4ce1b38273ac48d8948073ddfb98a753e6be
MMahmudd <shametlab@gmai.com>
2023-05-29 05:05:35 -0700

    Raise a message task_6

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" A function that used to  raise a type of exception."""
+
+def raise_exception_msg(message=""):
+    """"Raise a NameError exception with a custom error message."""
+    raise NameError(message)
