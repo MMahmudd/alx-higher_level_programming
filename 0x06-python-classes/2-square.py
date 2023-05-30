1199cfe18e6fe4aaa71f751352f5a686c07c876e
MMahmudd <shametlab@gmai.com>
2023-05-30 16:14:44 -0700

    update task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-""" Square the nemae of class."""
+"""Defination of class sqr."""
 
 
 class Square:
+    """Represent_square."""
+
     def __init__(self, size=0):
-        """Initialization new_Square.
+        """Initialize a new_Square.
 
         Arguments:
             size (int): size of the new_square.
         """
         if not isinstance(size, int):
-            raise TypeError("size should be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size should be  >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
