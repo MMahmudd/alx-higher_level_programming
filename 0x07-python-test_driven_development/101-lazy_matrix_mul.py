90ce29aa93fcd331d5d86cf9bbc4ded7fec57c0e
MMahmudd <shametlab@gmai.com>
2023-06-08 07:42:41 -0700

    updated

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -11,6 +11,4 @@ def lazy_matrix_mul(m_a, m_b):
         m_a (list of lists of ints/floats): The first_matrix.
         m_b (list of lists of ints/floats): The second_matrix.
     """
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("Matrix dimensions are incompatible for multiplication.")
     return (my_numpy.matmul(m_a, m_b))
