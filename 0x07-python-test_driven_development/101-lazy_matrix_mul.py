a94a299d08b10e0824816288b1b8fd08522f0a4a
MMahmudd <shametlab@gmai.com>
2023-06-08 06:59:45 -0700

    updated the error

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 101-lazy_matrix_mul.py
-"""Defination of a function matrix multiplication use NumPy."""
+"""Definition of a function matrix multiplication use NumPy."""
 import numpy as my_numpy
 
 
@@ -11,5 +11,6 @@ def lazy_matrix_mul(m_a, m_b):
         m_a (list of lists of ints/floats): The first_matrix.
         m_b (list of lists of ints/floats): The second_matrix.
     """
-
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("Matrix dimensions are incompatible for multiplication.")
     return (my_numpy.matmul(m_a, m_b))
