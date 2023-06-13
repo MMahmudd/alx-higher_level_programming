e69d4e45f79b0021902fd689223ebac13e93198d
MMahmudd <shametlab@gmai.com>
2023-06-13 11:52:52 -0700

    update task_1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""A module that contained a function named read_file"""
 
 
 def read_file(filename=""):
-    """Function that reads a text file and prints its content
-    Read the entire file content. Print the content to
-    stdout without adding an extra newline character"""
+    """Reads a text_file and printout to stdout.
 
-    with open(filename, encoding="utf-8") as f_object:
-        print(f_object.read(), end="")
+    Argumnets:
+        filename (str, optional): is named of text_file to read.
+        Default to "".
+    """
+    with open(filename, 'r', encoding="utf-8") as f_object:
+        read_text_file = f_object.read()
+        print(read_text_file, end='')
