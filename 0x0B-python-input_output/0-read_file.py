067167e8d4028125af79c83846f586baa3eca041
MMahmudd <shametlab@gmai.com>
2023-06-13 08:53:33 -0700

    update task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,6 @@
 def read_file(filename=""):
     """Function that reads a text file and prints its content."""
 
-    with open(filename, "r", encoding="utf-8") as file:
-        # Read the entire file content
-        text = file.read()
-        print(text, end="")
+    with open(filename, "r", encoding="utf-8") as f:
+        text_file = f.read()
+        print(text_file, end="")
