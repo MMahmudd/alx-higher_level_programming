b425b270463e2d5b7af0d94c2650c423d9e4b14a
MMahmudd <shametlab@gmai.com>
2023-06-13 05:56:39 -0700

    update task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
+#!/usr/bin/python3
 def read_file(filename=""):
-    # Open the file in read mode with UTF-8 encoding
-    with open(filename, "r", encoding="utf-8") as file:
-        # Iterate over each line in the file
-        for line in file:
-            # Print each line to stdout without
-            #adding an extra newline character
-            print(line, end="")
+    """"A function that used to read a text_file
+    and print out its content"""
+
+    with open(filename) as ff:
+        text_file = ff.read()
+        print(text_file, end="")
