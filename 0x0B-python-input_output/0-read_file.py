888b4f5e2a07fa602ce729ceee8b5566ec561cef
MMahmudd <shametlab@gmai.com>
2023-06-13 09:54:40 -0700

    updated task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@ def read_file(filename=""):
     Read the entire file content. Print the content to
     stdout without adding an extra newline character"""
 
-    with open(filename, "r", encoding="utf-8") as f:
-        text_file = f.read()
-        print(text_file, end="")
+    with open(filename, "r", encoding="utf-8") as f_object:
+        text = f_object.read()
+        print(text, end="")
