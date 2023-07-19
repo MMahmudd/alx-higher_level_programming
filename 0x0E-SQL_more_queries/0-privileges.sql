44e514c4b6044cfa84dc8a830e28d3a46bd6e651
MMahmudd <shametlab@gmail.com>
2023-07-19 14:14:02 -0700

    updated task_0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,11 +1,4 @@
 -- Show_privileges_for user_0d_1
-SELECT *
-FROM mysql.user
-WHERE User = 'user_0d_1'
-  AND Host = 'localhost';
-
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 -- Show_privileges_for user_0d_2
-SELECT *
-FROM mysql.user
-WHERE User = 'user_0d_2'
-  AND Host = 'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
