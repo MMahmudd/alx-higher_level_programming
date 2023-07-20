964fdecfc34b6be3c252b07675ed9a2985e6f91c
MMahmudd <shametlab@gmail.com>
2023-07-20 07:12:39 -0700

    update tasks

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,11 +1,8 @@
--- Query: List all shows from the hbtn_0d_tvshows_rate database, grouped by their rating sum.
--- Each record should display: TV Show Title and Rating Sum.
--- Results must be sorted in ascending order by the rating sum.
--- The database name "hbtn_0d_tvshows_rate" will be passed as an argument of the mysql command.
+-- lists_all shows_from hbtn_0d_tvshows_rate by_their_rating.
 
-SELECT tv_shows.title AS 'TV Show Title', SUM(tv_show_ratings.rate) AS 'Rating Sum'
-FROM hbtn_0d_tvshows_rate.tv_shows
-INNER JOIN hbtn_0d_tvshows_rate.tv_show_ratings
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+INNER JOIN tv_show_ratings
 ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.id
-ORDER BY `Rating Sum` ASC;
+ORDER BY rating DESC;
