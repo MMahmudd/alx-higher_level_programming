9cb1eac1afaf334dffbe362a39fc41ab6c6ca0de
MMahmudd <shametlab@gmail.com>
2023-07-20 03:21:07 -0700

    updated task

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,3 @@
 -- Query: List_all_records from "second_table" in_"hbtn_0c_0" database.
---display_ "score" and "name," ordered_by "score"_descending.
 
 SELECT score, name FROM second_table ORDER BY score DESC;
