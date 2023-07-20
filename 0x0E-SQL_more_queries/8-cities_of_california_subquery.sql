f6190f1f851eefbb22b7272703d001356b0e20e6
MMahmudd <shametlab@gmail.com>
2023-07-20 06:27:45 -0700

    updated tasks

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,3 @@
--- Query: List all the cities of California from the database hbtn_0d_usa.
--- The states table contains only one record with the name "California".
--- The results must be sorted in ascending order by cities.id.
--- The database name (hbtn_0d_usa) will be passed as an argument of the mysql command.
+-- lists all_the cities_that_can be_found in_the_database hbtn_0d_usa.
 
-SELECT id, name FROM hbtn_0d_usa.cities
-WHERE state_id = (
-      SELECT id FROM hbtn_0d_usa.states
-      WHERE name = "California")
-ORDER BY id;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
