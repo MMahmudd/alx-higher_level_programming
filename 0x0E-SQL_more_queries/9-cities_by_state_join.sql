5994aba16eb454e2f5e089555cb32dd7d84f778b
MMahmudd <shametlab@gmail.com>
2023-07-20 06:30:40 -0700

    updated task

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,6 @@
--- Query: List all cities from the database hbtn_0d_usa.
--- Each record should display: City ID, City Name, and State Name.
--- Results must be sorted in ascending order by City ID.
--- The database name "hbtn_0d_usa" will be passed as an argument of the mysql command.
+-- lists_all cities_contained in_the database_hbtn_0d_usa.
 
-SELECT cities.id AS 'City ID', cities.name AS 'City Name', states.name AS 'State Name'
-FROM hbtn_0d_usa.cities
-JOIN hbtn_0d_usa.states ON cities.state_id = states.id
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id=states.id
 ORDER BY cities.id;
