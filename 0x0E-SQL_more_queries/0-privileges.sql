bb90d0bdb14d642e080837968c8268597d1a3221
MMahmudd <shametlab@gmail.com>
2023-07-20 04:34:26 -0700

    updated tasks

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
+-- Showing all_privileges_of the_MySQL users user_0d_1& user_0d_2 on_server.
 
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
