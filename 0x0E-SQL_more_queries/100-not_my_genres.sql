43e432dc8efe6a3c62bda89a3be1ed267262a023
MMahmudd <shametlab@gmail.com>
2023-07-20 07:05:26 -0700

    update task

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,13 +1,13 @@
--- Query: List all genres not linked to the TV show "DEXTER" from the database.
--- Each record should display: Genre Name.
--- Results must be sorted in ascending order by the genre name.
--- The database name will be passed as an argument of the mysql command.
+-- list all_genres not_linked_to the_show_Dexter.
 
-SELECT tv_genres.name AS 'Genre Name'
-FROM hbtn_0d_tvshows.tv_genres
-WHERE tv_genres.id NOT IN (
-      SELECT tv_genres.id FROM hbtn_0d_tvshows.tv_genres
-      JOIN hbtn_0d_tvshows.tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-      JOIN hbtn_0d_tvshows.tv_shows ON tv_shows.id = tv_show_genres.show_id
-      WHERE tv_shows.title = 'DEXTER')
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.id NOT IN
+(SELECT tv_genres.id
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter")
 ORDER BY tv_genres.name;
