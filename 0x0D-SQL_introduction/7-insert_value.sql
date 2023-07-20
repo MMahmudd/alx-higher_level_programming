1c7fa07d11f7ee77702a6ba1b38632c66b90790c
MMahmudd <shametlab@gmail.com>
2023-07-20 02:47:00 -0700

    updated task

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- lists_all rows_of_the_table first_table from the_database hbtn_0c_0 in_my_MySQL_server.
+-- lists all rows_of_the_table first_table from the_database hbtn_0c_0 in_my_MySQL server.
 
 SELECT * FROM first_table;
