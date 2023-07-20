ea942498b01eded41d4d2b8c77b60945bfa27eac
MMahmudd <shametlab@gmail.com>
2023-07-20 06:13:35 -0700

    updated task

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,4 @@
--- Create the MySQL server user 'user_0d_1' if it doesn't already exist
+--create user.sql user_0d_1 should_have all_privileges on_my_ MySQL_server
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to 'user_0d_1' on all databases and tables
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
-
--- Refresh the MySQL privileges
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
