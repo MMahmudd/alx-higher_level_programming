ec7203ed2df879fe8249e8523ebc108b5ce14db7
MMahmudd <shametlab@gmail.com>
2023-07-20 04:14:35 -0700

    updated task_0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- Show privileges for the user user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Show all_privileges _for the_user of user_0d_1 & user_0d_2 o_my_server
 
--- Show privileges for the user user_0d_2
+HOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
