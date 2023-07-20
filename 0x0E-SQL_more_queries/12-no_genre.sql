b0e2586bfdb3d10a399653f19b500a4fd21c5a0f
MMahmudd <shametlab@gmail.com>
2023-07-20 06:42:55 -0700

    updat task

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,10 +1,8 @@
--- Query: List all shows from the hbtn_0d_tvshows database.
--- Each record should display: TV Show Title and Genre ID.
--- Results must be sorted in ascending order by TV Show Title and Genre ID.
--- If a show doesn’t have a genre, display NULL.
--- The database name "hbtn_0d_tvshows" will be passed as an argument of the mysql command.
+-- lists all_shows contained_in hbtn_0d_tvshows without_a genre_linked.
 
-SELECT tv_shows.title AS 'TV Show Title', tv_show_genres.genre_id AS 'Genre ID'
-FROM hbtn_0d_tvshows.tv_shows
-LEFT JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
