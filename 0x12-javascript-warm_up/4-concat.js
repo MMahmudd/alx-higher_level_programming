285d2cb618bdd5eeee2c32b63447010e0df2d98c
MMahmudd <shametlab@gmail.com>
2023-08-14 23:55:43 +0000

    update task4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 if (process.argv.length >= 3) {
-  const args_res = process.argv[2].concat(' is ', process.argv[3]);
-  console.log(args_res);
+  const res = process.argv[2].concat(' is ', process.argv[3]);
+  console.log(res);
 } else {
-  const args_res = 'undefined'.concat(' is ', process.argv[3]);
-  console.log(args_res);
+  const res = 'undefined'.concat(' is ', process.argv[3]);
+  console.log(res);
 }
