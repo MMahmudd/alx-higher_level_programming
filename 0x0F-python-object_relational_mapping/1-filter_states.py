80675eff52cf11939c2e539ffe071ff741c0f1da
MMahmudd <shametlab@gmail.com>
2023-08-18 11:49:20 +0000

    update task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,24 @@
--- Create_states_table in hbtn_0e_0_usa with some_data
+#!/usr/bin/python3
+"""
+File: 1filter_states.py
+Author Mahmud M.Adem
+Desc: A script_that_lists_states with_a_name starting_with N
+        from_the_database hbtn_0e_0_usa.
+Date: 17_ august_2023
+"""
 
-CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
-USE hbtn_0e_0_usa;
-CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states
+                    WHERE BINARY name LIKE 'N%' ORDER BY id ASC""")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
