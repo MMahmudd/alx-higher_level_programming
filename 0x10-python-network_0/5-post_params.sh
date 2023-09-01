4e1a28a67c17e391b8639e9da6d5808f5ad67563
MMahmudd <shametlab@gmail.com>
2023-09-01 01:09:26 -0700

    update tasks

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # send_a_POST request to_the_passed URL using_curl,
-curl -s -X POST -d "email=test@gmail.com&subject=I" +
-            "will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
