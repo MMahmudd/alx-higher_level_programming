0c3069d80e1a48617dca011578a3ac1185a8b93d
MMahmudd <shametlab@gmail.com>
2023-09-01 00:55:52 -0700

    update tasks

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # send_a_POST request to_the_passed URL using_curl,
-# and_display_the body_of_the_response
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
